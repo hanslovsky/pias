383d549330582bd3cbe6a4b046beea69acea6bb3
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-02-12 17:21:22 -0500

    Add first edge label communication methods

--- pias/solver_server.py
+++ pias/solver_server.py
@@ -17,12 +17,36 @@ _NO_SOLUTION_AVAILABLE = 1
 _USE_BIG_ENDIAN  = True
 _ENDIANNESS      = '>' if _USE_BIG_ENDIAN else '<'
 _INTEGER_PATTERN = f'{_ENDIANNESS}i'
+_UINT64_PATTERN  = f'{_ENDIANNESS}Q'
+_EDGE_PATTERN    = 'QQi'
+
+_SET_EDGE_REP_SUCCESS           = 0
+_SET_EDGE_REP_DO_NOT_UNDERSTAND = 1
+
+_SET_EDGE_REQ_EDGE_LIST         = 0
+
 
 def _int_as_bytes(number):
     return struct.pack(_INTEGER_PATTERN, number)
 
 def _bytes_as_int(b):
     return struct.unpack(_INTEGER_PATTERN, b)
+
+def _edges_as_bytes(edges):
+    pattern = f'{_ENDIANNESS}%s' % (_EDGE_PATTERN * len(edges))
+    return struct.pack(pattern, *tuple(e for edge in edges for e in edge))
+
+def _bytes_as_edges(b):
+    # 8 + 8 + 4
+    # label1, label2, 1 or 0
+    # uint64,uint64,byte
+    entry_size = 20
+    a = len(b)
+    assert len(b) % entry_size == 0
+    num_edges = len(b) // entry_size
+    pattern = f'{_ENDIANNESS}%s' % (_EDGE_PATTERN * num_edges)
+    e = struct.unpack(pattern, b)
+    return tuple((e[i+0], e[i+1], e[i+2]) for i in range(0, len(e), 3))
 
 def _ndarray_as_bytes(ndarray):
     # java always big endian
@@ -61,23 +85,38 @@ class SolverServer(object):
                 socket.send_more(_int_as_bytes(_SUCCESS))
                 socket.send(_ndarray_as_bytes(solution))
 
-        def set_edge_labels(message, socket):
-            pass
+        def set_edge_labels_receive(socket):
+            method = _bytes_as_int(socket.recv())
+            bytez  = socket.recv()
+            return method[0], bytez
+
+        def set_edge_labels_send(message, socket):
+            method = message[0]
+            if method == _SET_EDGE_REQ_EDGE_LIST:
+                labels = _bytes_as_edges(message[1])
+                socket.send_multipart(msg_parts=(_int_as_bytes(_SET_EDGE_REP_SUCCESS), _int_as_bytes(len(labels))))
+            else:
+                socket.send_multipart(msg_parts=(_int_as_bytes(_SET_EDGE_REP_DO_NOT_UNDERSTAND), _int_as_bytes(method)))
 
         self.ping_address             = '%s-ping' % address_base
         self.current_solution_address = '%s-current-solution' % address_base
+        self.set_edge_labels_address  = '%s-set-edge-labels' % address_base
 
         ping_socket                    = ReplySocket(self.ping_address, timeout=10)
         solution_notifier_socket       = PublishSocket('%s-new-solution' % address_base, timeout=10 / 1000, send=lambda socket, message: socket.sent_str(message)) # queue timeout is specified in seconds
         solution_request_socket        = ReplySocket(self.current_solution_address, timeout=10, respond=current_solution)
         # solution_update_request_socket = ReplySocket('%s-get-solution' % address_base, timeout=10, respond=update_request_received_confirmation)
-        # set_edge_labels_request_socket = ReplySocket('%s-set-edge-labels' % address_base, timeout=10, respond=set_edge_labels)
+        set_edge_labels_request_socket = ReplySocket(self.set_edge_labels_address, timeout=10, respond=set_edge_labels_send, receive=set_edge_labels_receive)
 
         workflow.add_solution_update_listener(lambda solution: solution_notifier_socket.queue.put(''))
 
 
         self.context = zmq.Context(io_threads=io_threads)
-        self.server  = Server(ping_socket, solution_notifier_socket, solution_request_socket)
+        self.server  = Server(
+            ping_socket,
+            solution_notifier_socket,
+            solution_request_socket,
+            set_edge_labels_request_socket)
 
         logging.info('Starting solver server at base address %s', address_base)
 
@@ -90,6 +129,9 @@ class SolverServer(object):
 
     def get_current_solution_address(self):
         return self.current_solution_address
+
+    def get_edge_labels_address(self):
+        return self.set_edge_labels_address
 
     def shutdown(self):
         # TODO handle things like saving etc in here
--- tests/cases/test_solver_server.py
+++ tests/cases/test_solver_server.py
@@ -1,5 +1,10 @@
 from __future__ import print_function
 
+import logging
+import time
+
+_logger = logging.getLogger(__name__)
+
 import contextlib
 import os
 import shutil
@@ -11,7 +16,7 @@ import z5py
 import zmq
 
 from pias import SolverServer
-from pias.solver_server import _bytes_as_int, _NO_SOLUTION_AVAILABLE
+from pias.solver_server import _bytes_as_int, _int_as_bytes, _edges_as_bytes, _NO_SOLUTION_AVAILABLE, _SET_EDGE_REQ_EDGE_LIST, _SET_EDGE_REP_SUCCESS, _SET_EDGE_REP_DO_NOT_UNDERSTAND
 
 
 @contextlib.contextmanager
@@ -49,7 +54,7 @@ def _mk_dummy_edge_data(
     f.create_dataset(edge_dataset, data=edges, dtype=np.uint64)
     f.create_dataset(edge_feature_dataset, data=features)
 
-    return labels
+    return edges, features, labels
 
 
 class TestSolverServerPing(unittest.TestCase):
@@ -95,8 +100,55 @@ class TestSolverCurrentSolution(unittest.TestCase):
             current_solution_socket.connect(server.get_current_solution_address())
             current_solution_socket.send_string('')
             solution_bytes = current_solution_socket.recv()
+            _logger.debug('%d solution bytes: %s', len(solution_bytes), solution_bytes)
             solution = _bytes_as_int(solution_bytes)
             self.assertEqual(1, len(solution))
             self.assertEqual(_NO_SOLUTION_AVAILABLE, solution[0])
+
+            server.shutdown()
+
+class TestSolverSetEdgeLabels(unittest.TestCase):
+
+    def test(self):
+
+        with _tempdir() as tmpdir:
+            address_base = 'inproc://address'
+            container    = os.path.join(tmpdir, 'edge-group')
+            edges, _, labels = _mk_dummy_edge_data(container)
+            server = SolverServer(
+                address_base=address_base,
+                edge_n5_container=container)
+
+            edge_label_socket = server.context.socket(zmq.REQ)
+            edge_label_socket.setsockopt(zmq.SNDTIMEO, 30)
+            edge_label_socket.setsockopt(zmq.RCVTIMEO, 30)
+            edge_label_socket.connect(server.get_edge_labels_address())
+            edge_label_socket.send(_int_as_bytes(_SET_EDGE_REQ_EDGE_LIST), flags=zmq.SNDMORE)
+            edge       = (edges[0, 0].item(), edges[0, 1].item(), labels[0])
+            edge_bytes = _edges_as_bytes((edge,))
+            edge_label_socket.send(edge_bytes)
+            response_code = _bytes_as_int(edge_label_socket.recv())
+            self.assertEqual(1, len(response_code))
+            self.assertEqual(_SET_EDGE_REP_SUCCESS , response_code[0])
+            num_edges = _bytes_as_int(edge_label_socket.recv())
+            self.assertEqual(1, len(num_edges))
+            self.assertEqual(1, num_edges[0])
+
+            edges_as_tuples = tuple((e[0].item(), e[1].item(), l) for e, l in zip(edges, labels))
+            edge_label_socket.send_multipart(msg_parts=(_int_as_bytes(_SET_EDGE_REQ_EDGE_LIST), _edges_as_bytes(edges_as_tuples)))
+            response_code = _bytes_as_int(edge_label_socket.recv())
+            self.assertEqual(1, len(response_code))
+            self.assertEqual(_SET_EDGE_REP_SUCCESS, response_code[0])
+            num_edges = _bytes_as_int(edge_label_socket.recv())
+            self.assertEqual(1, len(num_edges))
+            self.assertEqual(len(labels), num_edges[0])
+
+            edge_label_socket.send_multipart(msg_parts=(_int_as_bytes(-1), b''))
+            response_code = _bytes_as_int(edge_label_socket.recv())
+            self.assertEqual(1, len(response_code))
+            self.assertEqual(_SET_EDGE_REP_DO_NOT_UNDERSTAND, response_code[0])
+            message = _bytes_as_int(edge_label_socket.recv())
+            self.assertEqual(1, len(message))
+            self.assertEqual(-1, message[0])
 
             server.shutdown()
